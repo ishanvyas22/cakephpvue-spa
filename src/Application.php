0bd38b333ff6ae9c99a63c4faa82ee9028baabac
Ishan Vyas <isvyas@gmail.com>
2019-10-26 18:18:32 +0530

    Load Mix plugin

--- src/Application.php
+++ src/Application.php
@@ -35,6 +35,8 @@ class Application extends BaseApplication
      */
     public function bootstrap()
     {
+        $this->addPlugin('Mix', ['bootstrap' => true]);
+
         // Call parent to load bootstrap from files.
         parent::bootstrap();
 
