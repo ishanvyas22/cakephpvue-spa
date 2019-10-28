37b14b71e5855e995ad4e4467b7072f94a4fb27e
Ishan Vyas <isvyas@gmail.com>
2019-10-28 17:27:36 +0530

    CSS changes into mix config file

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,10 @@ const mix = require('laravel-mix');
 
 mix.setPublicPath('./webroot')
   .js('resources/js/app.js', 'webroot/js')
+  .combine([
+    'resources/css/base.css',
+    'resources/css/foundation-icons.css',
+    'resources/css/style.css',
+  ], 'webroot/css/main.css')
+  .copyDirectory('resources/css/foundation-icons', 'webroot/css/foundation-icons')
   .version();
